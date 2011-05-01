5bd76b5e33827df52fed13490464d2aa7083f942
Shadez <d.master.me@gmail.com>
2011-05-02 00:05:20 +0900

    [37] Cleanup & missed files.

--- includes/revision_nr.php
+++ includes/revision_nr.php
@@ -1,5 +1,5 @@
 <?php
-define('WOW_REVISION', 36);
+define('WOW_REVISION', 37);
 define('RUNONCE_REVISION', 9);
 define('DB_VERSION', 1);
 ?>
--- includes/templates/wow/wow_block_user_metal.php
+++
@@ -1,1 +0,0 @@
-<div class="user-plate ajax-update user-meta meta-login"><?php echo WoW_Locale::GetString('template_userbox_auth_caption'); ?></div>
